d1e79a971f94944e3aacf01b15daaa7abb847127
Jazzelle Wolverson <jazzelle.wolverson@gmail.com>
2026-05-19 09:00:59 -0400

    completed messages and usernames

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 server.listen(3000, () => {
     console.log('started on: http://localhost:3000');
@@ -19,7 +20,11 @@ const io = new Server(server);
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
     socket.on('chat message', (data) => {
-        console.log('Message Received: ${data}');
-        io.emit('chat message', data);
+        console.log(`Message Received: ${data}`);
+        io.emit('chat message', {username: socket.username, message: data});
+    });
+
+    socket.on("username", (data) => {
+        socket.username = data;
     })
 });
